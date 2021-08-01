0f101642fa1aef3323a8071cdafef1d91d9aa5c3
joshua211 <lycosh211@gmx.de>
2021-08-01 22:59:21 +0200

    add web

---
+++ Web/Persistence/LocalPersistence.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Web.Persistence
+{
+    public class LocalPersistence : IValuePersistence
+    {
+        private readonly IServiceScopeFactory factory;
+
+        public LocalPersistence(IServiceScopeFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        public async Task<string> GetValueAsync(string name)
+        {
+            using var scope = factory.CreateScope();
+            var localStorage = scope.ServiceProvider.GetService<ILocalStorageService>();
+            return await localStorage.GetItemAsStringAsync(name);
+        }
+        
+
+        public async Task StoreValueAsync(string name, string value)
+        {
+            using var scope = factory.CreateScope();
+            var localStorage = scope.ServiceProvider.GetService<ILocalStorageService>();
+            await localStorage.SetItemAsStringAsync(name, value);
+        }
+
+        public async Task ClearAsync()
+        {
+            using var scope = factory.CreateScope();
+            var localStorage = scope.ServiceProvider.GetService<ILocalStorageService>();
+            await localStorage.ClearAsync();
+        }
+
+        public async Task ClearAsync(string name)
+        {
+            using var scope = factory.CreateScope();
+            var localStorage = scope.ServiceProvider.GetService<ILocalStorageService>();
+            await localStorage.RemoveItemAsync(name);
+        }
+    }
+}
